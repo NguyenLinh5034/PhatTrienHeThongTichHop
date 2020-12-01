128813baf98af0f47f30adfff16e4cf0b2151a00
NguyenLinh5034 <72609087+NguyenLinh5034@users.noreply.github.com>
2020-12-01 10:11:42 +0700

    Add files via upload

---
+++ CalculatorClient.java
@@ -0,0 +1,17 @@
+import java.rmi.Naming;
+
+public class CalculatorClient {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Finding object...");
+            Calculator c = (Calculator)Naming.lookup("rmi://localhost:7777/MyCalculator");
+
+            System.out.println(c.addNum(12, 13));
+		System.out.println(c.tru(12, 13));
+System.out.println(c.nhan(12, 13));
+System.out.println(c.chia(12, 13));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
